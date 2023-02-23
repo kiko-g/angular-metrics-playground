8ce4b6af77e8cb9acd5942597ddb2a774d58d66d
kiko-g <kikojpgoncalvesgmail.com>
2023-02-23 17:30:53 +0000

    add: angular starter

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0d9488",
+        secondary: "#3b82f6",
+        navy: "#1a202c",
+        dark: "#242936",
+        darker: "#1e222c",
+        darkest: "#1a1e28",
+        darkish: "#333640",
+        light: "#f2f4f7",
+        lighter: "#f7f7f7",
+        lightest: "#fcfcfc",
+        lightish: "#ebedf0",
+      },
+      maxWidth: {
+        screen: "100vw",
+      },
+      fontSize: {
+        xxs: "0.6rem",
+      },
+      fontFamily: {
+        prose: ["Inter", ...defaultTheme.fontFamily.sans],
+        headings: ["Lexend", ...defaultTheme.fontFamily.sans],
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+        source: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        "ubuntu-mono": ["Ubuntu Mono", ...defaultTheme.fontFamily.mono],
+        system: defaultTheme.fontFamily.sans,
+        flow: "Flow",
+      },
+    },
+  },
+  plugins: [],
+};
