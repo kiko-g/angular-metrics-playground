0e0dda45512f085bc499f9e0dcd8ee531f2568dc
kiko-g <kikojpgoncalvesgmail.com>
2023-03-17 15:42:26 +0000

    update: tab and step tracking attempt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { MatomoTracker } from "@ngx-matomo/tracker";
+import { EventEmitter } from "@angular/core";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -8,15 +10,6 @@ import { MatomoTracker } from "@ngx-matomo/tracker";
 })
 export class AppComponent implements OnInit {
   constructor(private readonly matomoTracker: MatomoTracker) {}
-
-  ngOnInit() {
-    this.matomoTracker.trackPageView();
-    this.matomoTracker.trackEvent("AppComponent", "Clicked", "Label", this.tab);
-    this.matomoTracker.trackEvent("AppComponent", "Clicked", "Step", this.step);
-    this.matomoTracker.trackEvent("AppComponent", "completed", "true");
-    this.matomoTracker.setCustomVariable(1, "Tab", this.tab.toString(), "page");
-    this.matomoTracker.setCustomVariable(2, "Step", this.step.toString(), "event");
-  }
 
   year = new Date().getFullYear();
   title = "Angular Metrics Playground";
@@ -28,8 +21,45 @@ export class AppComponent implements OnInit {
   steps = ["A", "B", "C", "D", "E"];
   tabs = ["heroBolt", "heroChartPie", "heroCodeBracketSquare", "heroLightBulb", "heroPaperClip"];
 
+  // Add these methods
+  trackStepChange(step: number) {
+    this.matomoTracker.trackEvent("AppComponent", "StepChange", "Step", step);
+    this.matomoTracker.setCustomVariable(2, "Step", step.toString(), "event");
+  }
+
+  trackTabChange(tab: number) {
+    this.matomoTracker.trackEvent("AppComponent", "TabChange", "Tab", tab);
+    this.matomoTracker.setCustomVariable(1, "Tab", tab.toString(), "page");
+  }
+
+  stepChange = new EventEmitter<number>();
+  tabChange = new EventEmitter<number>();
+  private subscription = new Subscription();
+
+  ngOnInit() {
+    this.matomoTracker.trackPageView();
+
+    // Subscribe to stepChange and tabChange events
+    this.subscription.add(
+      this.stepChange.subscribe((step) => {
+        this.trackStepChange(step);
+      })
+    );
+
+    this.subscription.add(
+      this.tabChange.subscribe((tab) => {
+        this.trackTabChange(tab);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   changeTab(tabIndex: number) {
     this.tab = tabIndex;
+    this.tabChange.emit(this.tab); // Emit the event
   }
 
   nextStep() {
@@ -39,16 +69,19 @@ export class AppComponent implements OnInit {
       this.step += 1;
       if (this.step === this.steps.length - 1) this.completed = true;
     } else this.submitted = true;
+
+    this.stepChange.emit(this.step); // Emit the event
   }
 
   prevStep() {
     if (this.submitted) return;
-
     if (!this.completed && this.step !== 0) {
       this.step -= 1;
     } else if (this.completed) {
       this.step -= 1;
       this.completed = false;
     }
+
+    this.stepChange.emit(this.step); // Emit the event
   }
 }
